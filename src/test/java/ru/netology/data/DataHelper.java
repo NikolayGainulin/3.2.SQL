ba035247e3cfc07135521c7f6d8002f49503ae74
Nika test <nikolaygtest259@gmail.com>
2025-08-14 16:23:52 +0300

    initial commit

---
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -0,0 +1,50 @@
+package ru.netology.web.data;
+
+import com.github.javafaker.Faker;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Value;
+
+import java.util.Locale;
+import java.util.Random;
+
+public class DataHelper {
+    private DataHelper() {
+    }
+    private static final Faker FAKER = new Faker(new Locale("en"));
+
+
+    public static AuthInfo getAuthInfoWithTestData() {
+        return new AuthInfo("vasya", "qwerty123");
+    }
+    public static String generateRandomLogin() {
+        return FAKER.name().username();
+    }
+
+    public static String generateRandomPassword() {
+        return FAKER.internet().password();
+    }
+
+    public static AuthInfo generateRandomUser() {
+        return new AuthInfo(generateRandomLogin(), generateRandomPassword());
+    }
+
+    public static VerificationCode generateRandomVerificationCode() {
+        return new VerificationCode(FAKER.numerify("######"));
+    }
+
+    @Value
+    public static class AuthInfo {
+        private String login;
+        private String password;
+    }
+
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class VerificationCode {
+        String code;
+    }
+}
